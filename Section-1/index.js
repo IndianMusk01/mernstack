ab4d84996fb9af4c382d69b3c63902f3b0f88bdf
Ayush Singh <ayushsingh723585@gmail.com>
2024-02-16 13:55:45 +0530

    day8

---
+++ Section-1/index.js
@@ -0,0 +1,9 @@
+console.log("welcome to javascript");
+a=230;
+myname="Ayush "
+ishungry = true;
+empty = null;
+console.log(a);
+console.log(myname);
+console.log(typeof a);
+console.log(typeof (myname));
---
+++ Section-1/keywords.js
@@ -0,0 +1,6 @@
+if(34%2===0){
+    console.log("even");
+
+}else{
+    console.log("odd");
+}
